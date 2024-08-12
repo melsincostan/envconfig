9f3aa1cc0353b1c60e83e98676349e608460e032
Mélusine <melusine.hermier@proton.me>
2024-08-12 19:54:41 +0200

    Add parsing of string values

--- parsing.go
+++ parsing.go
@@ -3,6 +3,7 @@ package envconfig
 import (
 	"fmt"
 	"reflect"
+	"strings"
 )
 
 func parse[T any]() (*T, error) {
@@ -20,11 +21,25 @@ func parse[T any]() (*T, error) {
 		f_t := obj_t.Field(i)
 		f_v := obj_v.Field(i)
 
+		env_name, has_env_tag := f_t.Tag.Lookup("env")
+
+		if !has_env_tag {
+			env_name = strings.ToUpper(f_t.Name)
+		}
+
+		required := strings.ToLower(f_t.Tag.Get("binding")) == "required"
+
 		if !f_v.CanSet() {
 			return nil, fmt.Errorf("Field %s: not assignable", f_t.Name)
 		}
 
 		switch f_t.Type.Kind() {
+		case reflect.String:
+			res, err := getString(env_name, required)
+			if err != nil {
+				return nil, fmt.Errorf("field %s: %s", f_t.Name, err.Error())
+			}
+			f_v.SetString(res)
 		default:
 			return nil, fmt.Errorf("Field %s: unsupported type %s", f_t.Name, f_v.Kind())
 		}
